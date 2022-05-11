0be649381e135f2df98e8f3beea61df6fd384018
Ishmeet Singh Saggu <57167867+Psychik-N@users.noreply.github.com>
2022-05-12 01:13:41 +0530

    Update models.py

--- users/models.py
+++ users/models.py
@@ -8,3 +8,5 @@ class register_as(models.Model):
         ('Recruiter', 'Recruiter')
     )
     who = models.CharField(max_length=100, choices=REGISTER_TYPE)
+    bio = models.TextField(blank = True, null = True)
+    image = models.ImageField(null=True, blank=True, upload_to='profiles/', default="profiles/user-default.png")
