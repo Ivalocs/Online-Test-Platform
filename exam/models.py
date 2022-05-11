d3d0688925181844cad5f5e8a8c6e176c391cf3e
Ishmeet Singh Saggu <57167867+Psychik-N@users.noreply.github.com>
2022-05-12 01:17:08 +0530

    Update models.py

--- exam/models.py
+++ exam/models.py
@@ -11,3 +11,13 @@ class exam_details(models.Model):
 
     def __str__(self):
         return str(self.name)
+
+class exam_given(models.Model):
+    name = models.CharField(max_length=200, blank=True, null=True)
+    exam = models.CharField(max_length=200, blank=True, null=True)
+    correct = models.IntegerField(blank = True, null = True)
+    incorrect = models.IntegerField(blank = True, null = True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.exam)
