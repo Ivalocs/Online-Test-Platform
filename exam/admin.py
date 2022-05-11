497881cb0204e73dc45e211ab354149981bb8608
Ishmeet Singh Saggu <57167867+Psychik-N@users.noreply.github.com>
2022-05-12 01:36:32 +0530

    Update admin.py

--- exam/admin.py
+++ exam/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import exam_details
+from .models import exam_details, exam_given
 
 admin.site.register(exam_details)
 admin.site.register(exam_given)
